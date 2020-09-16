585aa65ea78a1882667e934437ded751e8bc434d
damiloyola <damiloyola@gmail.com>
2020-09-16 01:02:24 -0300

    js basico para el search form

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
+//API KEY : eDyIdYyGYjGzyjzBCvahgfJE97JU6hYJ
+
+//SEARCH BAR
+const urlSearch =
+  "https://api.giphy.com/v1/gifs/search?api_key=eDyIdYyGYjGzyjzBCvahgfJE97JU6hYJ&q=";
+const searchInput = document.getElementById("searchInput");
+
+const search = async (text) => {
+  const response = await fetch(urlSearch + text + "&limit=4");
+  const results = await response.json();
+  results.data.forEach((i) => {
+    console.log(i.title);
+  });
+};
+searchInput.addEventListener("input", () => search(searchInput.value));
+
 //MENU BURGER
 const btnBurger = document.querySelector(".menu__burguer");
 const ul = document.querySelector(".menu__ul");
