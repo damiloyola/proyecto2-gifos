04fa15eb630aaf35dd182bd4f3598b8fc5655555
damiloyola <damiloyola@gmail.com>
2020-09-17 23:56:14 -0300

    se empieza estilos y funcion js para mostrar resultado de busqueda

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ const searchInput = document.getElementById("searchInput");
 const ulSearch = document.querySelector(".search__recomend");
 const searchIcon = document.querySelector(".search__icon");
 
-const search = async (text) => {
+const searchBar = async (text) => {
   if (text === "") {
     ulSearch.classList.add("hide");
     searchInput.classList.remove("searching");
@@ -24,10 +24,11 @@ const search = async (text) => {
   results.data.forEach((i) => {
     const li = document.createElement("li");
     li.classList.add("recomend__result");
-    li.innerHTML = i.title;
+    li.innerHTML = `<a href=#>${i.title}</a>`;
     ulSearch.appendChild(li);
   });
 };
+
 searchIcon.addEventListener("click", () => {
   searchInput.value = "";
   searchIcon.src = "assets/icon-search.svg";
@@ -36,8 +37,16 @@ searchInput.addEventListener("focusout", () => {
   ulSearch.classList.add("hide");
   searchInput.classList.remove("searching");
 });
-searchInput.addEventListener("focus", () => search(searchInput.value));
-searchInput.addEventListener("input", () => search(searchInput.value));
+searchInput.addEventListener("focus", () => searchBar(searchInput.value));
+searchInput.addEventListener("input", () => searchBar(searchInput.value));
+
+//SEARCH RESULT
+
+const search = async (word) => {
+  const res = await fetch(urlSearch + word + "&limit=12");
+  const data = await res.json();
+  console.log(data);
+};
 
 //MENU BURGER
 const btnBurger = document.querySelector(".menu__burguer");
