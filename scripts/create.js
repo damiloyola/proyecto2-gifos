1c72e492ffe35f49ac58571abb0da49a57cb15db
damiloyola <damiloyola@gmail.com>
2020-10-11 21:20:39 -0300

    Crear gifo casi listo solo falta repetir captura

--- scripts/create.js
+++ scripts/create.js
@@ -1,12 +1,17 @@
 const $mainVideo = document.getElementById("main_video");
 const $mainGif = document.querySelector(".main_gif");
 const $mainOverlay = document.querySelector(".main_overlay");
+const $textOverlay = document.querySelector(".overlay_text");
+const $imgOverlay = document.querySelector(".overlay_img");
+const $btnsOverlay = document.querySelector(".overlay_btns");
+const $downloadOverBtn = document.querySelector(".btn_over_download");
 const $screenTitle = document.querySelector(".screen_title");
 const $screenParagraph = document.querySelector(".screen_paragraph");
 const $startBtn = document.querySelector(".button_start");
 const $recordBtn = document.querySelector(".button_record");
 const $stopBtn = document.querySelector(".button_stop");
 const $uploadBtn = document.querySelector(".button_upload");
+const $restartBtn = document.querySelector(".restartButton");
 const $step1 = document.querySelector(".step1");
 const $step2 = document.querySelector(".step2");
 const $step3 = document.querySelector(".step3");
@@ -45,6 +50,7 @@ const record = () => {
                 quality: 10,
                 width: 360,
                 hidden: 240,
+
                 onGifRecordingStarted: function () {
                     console.log("started");
                 },
@@ -68,10 +74,12 @@ const stopRecord = () => {
     $uploadBtn.style.display = "block";
     $mainVideo.classList.add("hide");
     $mainGif.classList.remove("hide");
+    $restartBtn.classList.remove("hide");
     recorder.stopRecording(() => {
         blob = recorder.getBlob();
-        $mainGif.src = URL.createObjectURL(blob);
-
+        const url = URL.createObjectURL(blob);
+        $mainGif.src = url;
+        $downloadOverBtn.onclick = () => downloadGif(url, "myGif");
         form.append("file", blob, "myGif.gif");
     });
 };
@@ -82,16 +90,22 @@ const uploadRecord = async () => {
     $step2.classList.remove("active");
     $step3.classList.add("active");
     $mainOverlay.classList.remove("hide");
+    $uploadBtn.style.display = "none";
 
     await fetch(
         "https://upload.giphy.com/v1/gifs?api_key=eDyIdYyGYjGzyjzBCvahgfJE97JU6hYJ",
         {
+            mode: "cors",
             method: "POST",
             body: form,
         }
     )
         .then((res) => res.json())
         .then((gif) => {
+            $imgOverlay.src = "/assets/check.svg";
+            $textOverlay.innerHTML = "Subido con exito!";
+            $btnsOverlay.classList.remove("hide");
+
             console.log(gif);
         })
         .catch((err) => console.log(err));
